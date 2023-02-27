b7c7b454389dd95ea4d2ca2b5c2137c58f31f772
Baamki <sevenbumki@gmail.com>
2023-02-27 23:14:55 +0900

    Initial commit

---
+++ src/test/java/com/beom/bankingServer/BankingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beom.bankingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
