ef982f7755059b51aa1465e357ed2c9ee3ee48d0
Baamki <sevenbumki@gmail.com>
2023-02-28 23:19:30 +0900

    소스ㅡ틸

--- src/main/java/com/beom/bankingServer/BankingServerApplication.java
+++ src/main/java/com/beom/bankingServer/BankingServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankingServerApplication {
-
+		//소스트리
 	public static void main(String[] args) {
 		SpringApplication.run(BankingServerApplication.class, args);
 	}
